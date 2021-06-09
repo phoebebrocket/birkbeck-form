2f65259d507b19a2443632b249bf3967d95c2806
phoebebrocket <phoebebrocket@gmail.com>
2021-06-09 20:42:32 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Welcome to this form test
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Birkbeck Form
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
